a9e4fe9a98c12d0fc41ebc8691446a28fd544e65
sarahrami <sarahrami97@gmail.com>
2024-04-07 00:53:16 -0400

    config modification

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,33 +1,31 @@
 import { ApplicationConfig } from '@angular/core';
-import {myUrl} from './graphql.config'
+import { myUrl } from './graphql.config';
 import { provideRouter } from '@angular/router';
-import { APOLLO_OPTIONS, Apollo } from 'apollo-angular';
+import { APOLLO_OPTIONS } from 'apollo-angular';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { ApolloClientOptions, ApolloLink, InMemoryCache } from '@apollo/client';
-import { HttpLink  } from 'apollo-angular/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpLink } from 'apollo-angular/http';
 import { ModalServices } from './services/modal.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
+    provideRouter(routes),
     ModalServices,
     provideClientHydration(),
     provideHttpClient(),
     {
       provide: APOLLO_OPTIONS,
-      useFactory:(
+      useFactory: (
         httpLink: HttpLink
       ): ApolloClientOptions<unknown> => ({
         link: ApolloLink.from([
-          httpLink.create({uri: myUrl}),
+          httpLink.create({ uri: myUrl }),
         ]),
         cache: new InMemoryCache()
       }),
       deps: [HttpLink]
-    },
-    Apollo, provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()
+    }
   ]
 };
